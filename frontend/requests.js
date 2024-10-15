eb5c4d15441a0fd8c98f1a4699ecc10b22a0ac70
giansegg <gianpier.segovia@utec.edu.pe>
2024-10-14 21:44:09 -0500

    Terminado

--- frontend/requests.js
+++ frontend/requests.js
@@ -7,12 +7,17 @@ document
     const country1 = document.getElementById("country1").value;
     const city2 = document.getElementById("city2").value;
     const country2 = document.getElementById("country2").value;
-
     const option = document.getElementById("option").value;
 
-    fetch(
-      `http://127.0.0.1:5000/data?city1=${city1}&country1=${country1}&city2=${city2}&country2=${country2}&option=${option}`
-    )
+    // Verifica los valores
+    console.log(`City1: ${city1}, Country1: ${country1}`);
+    console.log(`City2: ${city2}, Country2: ${country2}`);
+    console.log(`Option: ${option}`);
+
+    const url = `http://127.0.0.1:5000/data?city1=${city1}&country1=${country1}&city2=${city2}&country2=${country2}&option=${option}`;
+    console.log(url);
+
+    fetch(url)
       .then((response) => {
         if (!response.ok) {
           throw new Error("Error en la solicitud");
